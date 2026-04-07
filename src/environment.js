d1607d1442ccfa2e0270e3bc851f536fde0dc134
nnxi <skwogur03@gmail.com>
2026-04-07 17:26:28 +0900

    fix : file path err

--- src/environment.js
+++ src/environment.js
@@ -34,7 +34,7 @@ const preProcess = (answers) => {
 
 const postProcess = async (targetPath) => {
     try {
-        const envExaplePath = path.join(targetPath, '.env.example.ejs');
+        const envExaplePath = path.join(targetPath, '.env.example');
         const envPath = path.join(targetPath, '.env');
 
         if (fs.existsSync(envExaplePath) && !fs.existsSync(envPath)) {
