1ae2ad9a36941911bb365a2ea7aa44a77212d12b
nnxi <skwogur03@gmail.com>
2026-04-22 18:42:47 +0900

    backup

--- src/question.js
+++ src/question.js
@@ -19,6 +19,26 @@ const getOptions = async () => {
       name: 'useJWT',
       message: 'Add JWT authentication boilerplate?',
       default: false
+    },
+    {
+      type: 'confirm',
+      name: 'useUsersDomain',
+      message: 'Include User(Register/Login) API boilerplate?',
+      when: (answers) => answers.useDB && answers.useJWT,
+      default: true 
+    },
+    {
+      type: 'confirm',
+      name: 'useSwagger',
+      message: 'Include Swagger API documentation setup?',
+      default: false
+    },
+    {
+      type: 'confirm',
+      name: 'useMonitoring',
+      message: 'Include swagger-stats real-time monitoring?',
+      when: (answers) => answers.useSwagger,
+      default: true
     }
   ]);
 
